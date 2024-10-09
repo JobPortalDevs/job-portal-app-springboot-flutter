85fe70e4972fac12eb8c1a66b7d891ffc7704cf7
ndourc <119441286+ndourc@users.noreply.github.com>
2024-10-09 19:16:02 +0200

    Update user_profile_service.dart

--- frontend/jobportal_app/lib/features/user_profile/services/user_profile_service.dart
+++ frontend/jobportal_app/lib/features/user_profile/services/user_profile_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
-
 import 'package:jobportal_app/constants/apis/urls.dart';
 import "package:http/http.dart" as http;
+import 'package:jobportal_app/core/utils/api_response.dart';
 import 'package:jobportal_app/features/user_profile/models/user_profile_model.dart';
 
 class UserProfileService {
@@ -29,6 +29,41 @@ class UserProfileService {
       return UserProfile.fromJson(jsonDecode(response.body));
     } else {
       throw Exception("Failed to create user profile");
+    }
+  }
+
+  Future<UserProfile> updateUserProfile(UserProfile profile) async {
+    final response = await http.put(
+      Uri.parse(userProfileBaseUrl),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(profile.toJson()),
+    );
+    if (response.statusCode == 200) {
+      return UserProfile.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to update user profile");
+    }
+  }
+
+  Future<ApiResponse> deleteUserProfile(UserProfile profile) async {
+    final response = await http.delete(
+      Uri.parse('$userProfileBaseUrl/${profile.id}'),
+      headers: {"Content-Type": "application/json"},
+    );
+    print('Deleting user profile at: $userProfileBaseUrl/${profile.id}');
+    if (response.statusCode == 204) {
+      return ApiResponse(
+        success: true,
+        data: null,
+        message: 'User profile deleted successfully',
+      );
+    } else {
+      return ApiResponse(
+        success: false,
+        data: null,
+        message:
+            'Failed to delete user profile. Status code: ${response.statusCode}',
+      );
     }
   }
 }
