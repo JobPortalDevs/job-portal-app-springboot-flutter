f8c604fd8a604d84705aab0839a47b255f717ccb
ndourc <119441286+ndourc@users.noreply.github.com>
2024-10-10 12:33:13 +0200

    Delete backend/job_portal_app directory

--- backend/job_portal_app/src/main/java/com/job_portal_app/job_portal_app/repository/UserProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.job_portal_app.job_portal_app.repository;
-
-import com.job_portal_app.job_portal_app.model.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-}
